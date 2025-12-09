1094060e8e6dc92c938286b44101c07bc102ecbe
kenzalittlegenius <kenza.benmansour@laposte.net>
2025-12-09 13:09:09 +0100

    The Project is finished.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,11 +2,19 @@
 public class Main {
     public static void main(String[] args){
         //creation of our object from the class we created
-        System.out.println("Hello here is the main");
+        Student kenza = new Student("Kenza", "Benmansour");
+        kenza.addResult("Programmation 2", new Grade(20));
+        kenza.addResult("Structures discrètes", new Grade(18));
 
-        Student Kenza = new Student("Kenza", "Benmansour");
-        Kenza.averageGrade();
-        Kenza.printResults();
+        Student alice = new Student("Alice", "Durand");
+        alice.addResult("Programmation 2", new Grade(15));
+        alice.addResult("Structures discrètes", new Grade(17));
+
+        Cohort cohort = new Cohort("L2 Informatique");
+        cohort.addStudent(kenza);
+        cohort.addStudent(alice);
+
+        cohort.printStudentsResults();
 
     }
 }
